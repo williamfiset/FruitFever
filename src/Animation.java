f2945d9cc039da5655b5efa603e8ceb6a6af315b
Micah Stairs <micah.stairs@gmail.com>
2014-04-22 13:45:47 -0300

    Added better documentation, a second constructor, and functionality to allow it to only play the animation once if instructed.

--- src/Animation.java
+++ src/Animation.java
@@ -11,13 +11,31 @@ import java.awt.*;
 
 public class Animation {
 
+	/** Public instance variables **/
 	public int x, y;
-	private GImage[] images;
 	public GImage currentImage;
+	public boolean doneAnimating = false;
+	
+	/** Private instance variables **/
+	private GImage[] images;
 	private int counter = 0, delayCounter = 0, delay;
-	private boolean reverse, counterGoingUp = true;
-
-	public Animation(int x, int y, GImage[] originalImages, boolean reverse, int delay){
+	private boolean reverse, counterGoingUp = true, repeat;
+	
+	
+	/**
+	 * @param x : default x-position
+	 * @param y : default y-position
+	 * @param originalImages : array of images for the animation
+	 * @param reverse (optional - default is false):
+	 * 	-If true, the images go in order: 0, 1, ...images.length-2, images.length-1, images.length-2, ...1, 0
+	 *	-If false, the images go in order: 0, 1, ...images.length-1 (and then it will begin at 0 again on next cycle)
+	 * @param delay (optional - default is 1): the higher this value is, the slower the image will animate
+	 * @param repeat (optional - default is true):
+	 *	-If true, the animation is played over and over
+	 *	-If false, the animation is played once
+	 **/
+	 
+	public Animation(int x, int y, GImage[] originalImages, boolean reverse, int delay, boolean repeat){
 
 		this.x = x;
 		this.y = y;
@@ -29,13 +47,21 @@ public class Animation {
 		
 		this.reverse = reverse;
 		this.delay = delay;
+		this.repeat = repeat;
 		
 		// Gives it a starting image so that it doesn't raise a NullPointerException
 		currentImage = new GImage(images[counter].getImage());
 		
 	}
 	
+	public Animation(int x, int y, GImage[] originalImages){
+		this(x, y, originalImages, false, 1, true);
+	}
+	
 	public void animate(){
+	
+		if(doneAnimating)
+			return;
 		
 		// Break out of this method if it's not time to change the image yet
 		if(++delayCounter < delay)
@@ -49,15 +75,26 @@ public class Animation {
 		else
 			counter--;
 		
-		// Determine if the counter needs to switch directions
+		/** Determine if the counter needs to switch directions **/
 		if(reverse){
-			if(counter == images.length - 1)
-				counterGoingUp = false;
-			else if(counter == 0)
-				counterGoingUp = true;
+			if(counter == images.length - 1){
+				if(!repeat)
+					doneAnimating = true;
+				else
+					counterGoingUp = false;
+			}
+			else if(counter == 0){
+				if(!repeat)
+					doneAnimating = true;
+				else
+					counterGoingUp = true;
+			}
 		}
-		else if (counter == images.length)
+		else if (counter == images.length){
+			if(!repeat)
+				doneAnimating = true;
 			counter = 0;
+		}
 		
 		// Switch the GImage to the correct index
 		currentImage.setImage(images[counter].getImage());
