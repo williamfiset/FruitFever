babc0637f4425dd51f41438e7fb7adb168bf7a3b
Micah Stairs <micah.stairs@gmail.com>
2014-04-24 11:04:02 -0300

    Added methods to adjust thhe bounding box of a Thing by a relative amount.

--- src/Thing.java
+++ src/Thing.java
@@ -44,6 +44,31 @@ public class Thing extends Rectangle{
 		this(x, y, Data.TILE_SIZE, Data.TILE_SIZE);	
 	}
 	
+	/** Set bounding box (does not adjust the image, only the underlying Rectangle **/
+	
+	public void offsetAllBounds(int left, int right, int top, int down){
+		offsetLeftBounds(left);
+		offsetRightBounds(right);
+		offsetTopBounds(top);
+		offsetBottomBounds(down);
+	}
+	
+	public void offsetLeftBounds(int offset){
+		setBounds(x + offset, y, width - offset, y);
+	}
+	
+	public void offsetRightBounds(int offset){
+		setBounds(x, y, width + offset, y);
+	}
+	
+	public void offsetTopBounds(int offset){
+		setBounds(x, y + offset, width, y - offset);
+	}
+	
+	public void offsetBottomBounds(int offset){
+		setBounds(x, y, width, y + offset);
+	}
+	
 	/** Update position **/
 	public void animate(){
 	
