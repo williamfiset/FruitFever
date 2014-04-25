2747b86c17c49455b0a4a718c0c6e288f616ced3
MicahAndWill <wafiset@mta.ca>
2014-04-24 21:52:41 -0300

    Added Player/Keyboard Input/Moved Load Hearts
    Fruit Fever now has startingPlayerX and startingPlayerY variables as
    well as a main character to some degree.

--- src/FruitFever.java
+++ src/FruitFever.java
@@ -13,12 +13,13 @@ import java.util.*;
 
 public class FruitFever extends GraphicsProgram implements MouseMotionListener {
 
-	protected final static int SCREEN_WIDTH = 700, SCREEN_HEIGHT = 500, MAIN_LOOP_SPEED = 30;
+	final static int SCREEN_WIDTH = 700, SCREEN_HEIGHT = 500, MAIN_LOOP_SPEED = 30;
 
 
 	static ArrayList<Block> blocks = new ArrayList<Block>();
 	static ArrayList<Thing> things = new ArrayList<Thing>();
 	static ArrayList<Button> buttons = new ArrayList<Button>();
+	static Player player;
 	static Button clickedOnButton = null;
 	
 	// 0 = Loading Game, 1 = Main Menu, 2 = Level Selection, 3 = Playing, 4 = Controls, 5 = Options, 6 = Multiplayer Playing
@@ -27,8 +28,9 @@ public class FruitFever extends GraphicsProgram implements MouseMotionListener {
 	static int viewX = 0, viewY = 0;
 	static int currentLevel = 0;
 
-	static int lives = 3, maxLives = 3;
-	static GImage[] livesImages = new GImage[maxLives]; 
+	static int playerStartX = 100, playerStartY= 100;
+
+	static GImage[] livesImages = new GImage[player.maxLives];
 	
 	@Override public void init() {
 		
@@ -37,7 +39,7 @@ public class FruitFever extends GraphicsProgram implements MouseMotionListener {
 		
 		// Renders Images in the Data class, and fills the object Arrays^
 		Data.loadImages();
-		
+
 		// Adds main menu buttons to the ArrayList
 		for(int i = 0; i < 4; i++)
 			buttons.add(new Button((int) (FruitFever.SCREEN_WIDTH/2 - Data.menuImages[i/3].getWidth()/2), 100 + 75*i, i, Data.menuImages[3*i], Data.menuImages[3*i + 1], Data.menuImages[3*i + 2]));
@@ -65,7 +67,9 @@ public class FruitFever extends GraphicsProgram implements MouseMotionListener {
 				/** Blocks **/
 				for(Block obj : blocks)
 					obj.image.setLocation(obj.x - viewX, obj.y - viewY);
-			
+					
+				player.motion();
+
 			}
 			
 			pause(MAIN_LOOP_SPEED);
@@ -73,7 +77,19 @@ public class FruitFever extends GraphicsProgram implements MouseMotionListener {
 		
 	}
 	
-	@Override public void keyPressed(KeyEvent key){}
+	@Override public void keyPressed(KeyEvent key){
+
+		int keyCode = key.getKeyCode();
+
+		switch (keyCode) {
+			case KeyEvent.VK_A: player.dx = -Player.horizontalVelocity; break;
+			case KeyEvent.VK_D: player.dx =  Player.horizontalVelocity; break;
+			case KeyEvent.VK_W: player.dy = -Player.verticalVelocity; break;
+			case KeyEvent.VK_S: player.dy =  Player.verticalVelocity; break;
+		}
+			
+
+	}
 	
 	@Override public void keyReleased(KeyEvent key){}
 	
@@ -93,7 +109,6 @@ public class FruitFever extends GraphicsProgram implements MouseMotionListener {
 		}		
 	
 	}
-	
 	@Override public void mouseDragged(MouseEvent mouse) {
 	
 		/** Check to see if the mouse is on the selected button or not and sets the image accordingly **/			
@@ -163,7 +178,7 @@ public class FruitFever extends GraphicsProgram implements MouseMotionListener {
 
 /** Loads and Displays all initial graphics of a level on the screen  **/
 	private void loadLevel(){
-	
+		
 		// Clear the screen
 		removeAll();
 
@@ -175,13 +190,18 @@ public class FruitFever extends GraphicsProgram implements MouseMotionListener {
 
 		// Loads all Blocks and Things
 		Data.loadObjects("../levels/levels.txt", currentLevel);
-		
+
+
 		// Displays all blocks on-screen
 		for(Block obj : blocks){
 			obj.image.setLocation(obj.x, obj.y);
 			add(obj.image);
 		}
 		
+		// Creates the Player class
+		player = new Player(playerStartX, playerStartY, Data.playerTemp, false, 0, false, 0, 0 ) ;
+
+
 		
 		/** TESTING PURPOSES ONLY **/
 		things.add(new Animation(50, 50, Data.berryAnimation, true, 3, true));
@@ -190,33 +210,23 @@ public class FruitFever extends GraphicsProgram implements MouseMotionListener {
 		// things.add(new MovingAnimation(350, 50, Data.swirlAnimation, false, 1, false, 10, 5));
 		things.add(new Swirl(250, 50, 10, 5));
 		things.add(new BlueEnemy(175, 50, 0, 0));
+		things.add(player);
 		/** **/
+
+
+		// Draws initial objects on the screen
+		for(Thing obj : things){
+			obj.image.setLocation(obj.x, obj.y);
+			add(obj.image);
+		}
 		
-		// for(Thing obj : things){
-		// 	obj.image.setLocation(obj.x, obj.y);
-		// 	add(obj.image);
-		// }
-		
-		for(int i = 0; i < maxLives; i++){
+		// Loads the Hearts
+		for(int i = 0; i < player.maxLives; i++){
 			livesImages[i] = new GImage(Data.heartImage.getImage());
 			livesImages[i].setLocation(i*Data.TILE_SIZE, 0);
 			add(livesImages[i]);
 		}
+
 			
 	}
-	
-/** Adjusts the amount of lives that the player has, and redraws the hearts accordingly **/	
-	public static void adjustLives(int changeInLives){
-	
-		lives += changeInLives;
-		
-		for(int i = 0; i < maxLives; i++){
-			if(i < lives)
-				livesImages[i].setVisible(true);
-			else
-				livesImages[i].setVisible(false);
-		}
-	
-	}
-
 }
