1f7592220f18038892b4467fdd069501e32db272
MicahAndWill <wafiset@mta.ca>
2014-04-26 16:23:19 -0300

    working...

--- src/Player.java
+++ src/Player.java
@@ -61,38 +61,65 @@ class Player extends MovingAnimation {
 
 		// IMPORTANT: MAKE COLLISION DETECTIONS EASY WITH: Rectangle methods, contains() and , intersects()
 
-		// Contains all the surrounding blocks in a list, without doubly accounted blocks
-		HashSet<Block> surroundingBlocks = new HashSet<Block>();
-
-		// Gets all the blocks that are surrounding the Player
-		surroundingBlocks.add( Block.getBlock(imageX  - width/3, imageY - width/3 ) ); // NorthWest 
-		surroundingBlocks.add( Block.getBlock(imageX  + width/3, imageY - width/3 ) ); // North 
-		surroundingBlocks.add( Block.getBlock(imageX  + width + width/3, imageY - width/3 ) ); // NorthEast 
-
-		surroundingBlocks.add(Block.getBlock(imageX  + width + width/3, imageY + height/2)); // East
-		surroundingBlocks.add(Block.getBlock(imageX  - width/3, imageY + height/3)); // West
-
-		surroundingBlocks.add(Block.getBlock( imageX - width/3, imageY + height + height/3 )); // SouthWest
-		surroundingBlocks.add(Block.getBlock( imageX + width/3 , imageY + height + height/3 )); // South
-		surroundingBlocks.add(Block.getBlock( imageX + width + width/3, imageY + height + height/3)); // SouthEast
 
 
-		// remove the singular null reference (due to air blocks)
-		surroundingBlocks.remove(null); 
 
-		// System.out.println(surroundingBlocks.size());
 
-		// Verify the surrounding blocks for a collision
-		for (Block block : surroundingBlocks) {
+		for (Block block : FruitFever.blocks) {
 			if (block.intersects(this)) {
 				
-				if (block.imageX < imageY || block.imageY > imageY + height)
-					dy = 0;
-				else if (block.imageX < imageY || block.imageX > imageX + width)
-					dx = 0;
+				GRect rect = new GRect(block.x)
+
+				FruitFever.draw(new GRect(block.x, block.y) )
 
 			}
 		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// // Contains all the surrounding blocks in a list, without doubly accounted blocks
+		// HashSet<Block> surroundingBlocks = new HashSet<Block>();
+
+		// // Gets all the blocks that are surrounding the Player
+		// surroundingBlocks.add( Block.getBlock(imageX  - width/3, imageY - width/3 ) ); // NorthWest 
+		// surroundingBlocks.add( Block.getBlock(imageX  + width/3, imageY - width/3 ) ); // North 
+		// surroundingBlocks.add( Block.getBlock(imageX  + width + width/3, imageY - width/3 ) ); // NorthEast 
+
+		// surroundingBlocks.add(Block.getBlock(imageX  + width + width/3, imageY + height/2)); // East
+		// surroundingBlocks.add(Block.getBlock(imageX  - width/3, imageY + height/3)); // West
+
+		// surroundingBlocks.add(Block.getBlock( imageX - width/3, imageY + height + height/3 )); // SouthWest
+		// surroundingBlocks.add(Block.getBlock( imageX + width/3 , imageY + height + height/3 )); // South
+		// surroundingBlocks.add(Block.getBlock( imageX + width + width/3, imageY + height + height/3)); // SouthEast
+
+
+		// // remove the singular null reference (due to air blocks)
+		// surroundingBlocks.remove(null); 
+
+		// // System.out.println(surroundingBlocks.size());
+
+		// // Verify the surrounding blocks for a collision
+		// for (Block block : surroundingBlocks) {
+		// 	if (block.intersects(this)) {
+				
+		// 		if (block.imageX < imageY || block.imageY > imageY + height)
+		// 			dy = 0;
+		// 		else if (block.imageX < imageY || block.imageX > imageX + width)
+		// 			dx = 0;
+
+		// 	}
+		// }
 
 	}
 
