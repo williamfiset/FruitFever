d8bc57f8495c0c406b67fa70bfedcb295400269c
Micah Stairs <micah.stairs@gmail.com>
2014-05-26 21:32:02 -0300

    Formatting and grammer

--- src/InformationStorer.java
+++ src/InformationStorer.java
@@ -5,26 +5,25 @@
 * @Author William Fiset
 * March 15, 2014
 *
-* The InformationStorer class creates a .ser (Serialable) file that stores a hashmap 
+* The InformationStorer class creates a .ser (Serialable) file that stores a HashMap 
 * object that can contain key-value information. An InformationStorer object will let you easily 
-* access the information stored in it's file using addItem() and getItem(). The objects you 
-* store in the hashmap MUST implement 'Serializable' to be saved and reterived successfully. Also
-* note that getItem returns a genetric object meaning that you need to cast which type of object
-* your getting when your using that method.
+* access the information stored in its file using addItem() and getItem(). The objects you 
+* store in the HashMap MUST implement 'Serializable' to be saved and reterived successfully. Also
+* note that getItem() returns a generic object meaning that you need to cast which type of object
+* you're getting when you're using that method.
 *
 * ex:
 *
-*\ // Store Fruit object in file
+*	Store Fruit object in file:
 *
-*InformationStorer infoFile = new InformationStorer("fruitDataFile");
-*Fruit apple = new Fruit(); // Fruit class implements Serializable
-*infoFile.addItem("myFavFruit", apple);
+*		InformationStorer infoFile = new InformationStorer("fruitDataFile");
+*		Fruit apple = new Fruit(); // Fruit class implements Serializable
+*		infoFile.addItem("myFavFruit", apple);
 *
 *
-*\ // Get Fruit object in file
+*	Get Fruit object in file:
 *
-*Fruit aFruit = (Fruit) infoFile.getItem("myFavFruit");
-*
+*		Fruit aFruit = (Fruit) infoFile.getItem("myFavFruit");
 *
 *
 * NOTE: As deserialze has @SuppressWarnings("unchecked") attached to it, the class/method
@@ -35,49 +34,48 @@
 import java.io.*;
 import java.util.*;
 
-public class InformationStorer{
+public class InformationStorer {
 
     private String fileName;
     private Object defaultValue;
 
     /** This Hashmap gets serialized within the file and contains all the object you wish to save **/
-    private HashMap <String , Object> instanceDictionary = new HashMap <String, Object> ();
+    private HashMap<String, Object> instanceDictionary = new HashMap<String, Object>();
     
     // Needed for internal usage
-    private InformationStorer(){}
+    private InformationStorer() {}
 
     public InformationStorer(String fileName) {
         this.fileName = fileName;
     }
     
-    public InformationStorer(String fileName, String key, Object defaultValue){
+    public InformationStorer(String fileName, String key, Object defaultValue) {
         this.fileName = fileName;
         this.defaultValue = defaultValue;
         addItem(key, defaultValue);
     }
 
-    /** adds a new element to instanceDictionary & to fileName.ser **/
-    public void addItem(String key, Object value){
-
+    /** Adds a new element to instanceDictionary & to fileName.ser **/
+    public void addItem(String key, Object value) {
         instanceDictionary.put(key, value);
         serialize();
-
     }
 
-    /** Gets and element from the instanceDictionary **/
-    public Object getItem(String key){
+    /** Gets an element from the instanceDictionary **/
+    public Object getItem(String key) {
         
         deserialize();
 
-        if ( instanceDictionary.containsKey(key)) 
+        if (instanceDictionary.containsKey(key)) 
              return instanceDictionary.get(key);
 
-        System.out.println("Couldn't Find Key in HashMap: " + key);
+        System.out.println("Couldn't find Key in HashMap: " + key);
+		
         return defaultValue;
     }
 
     /** Checks if file exists in Directory **/
-    public static boolean fileExists(String file){
+    public static boolean fileExists(String file) {
         
         // Generic Object used to get class 
         InformationStorer obj = new InformationStorer();
@@ -86,14 +84,14 @@ public class InformationStorer{
     }
 
     /** Stores a byte HashMap Object into a file called: fileName.ser */
-    protected void serialize(){
+    protected void serialize() {
 
         try {
 
         FileOutputStream fileOut = new FileOutputStream(fileName + ".ser");
         ObjectOutputStream out = new ObjectOutputStream(fileOut);
          
-         // Store hashmap object in file
+         // Store HashMap object in file
         out.writeObject(instanceDictionary);
         out.close();
         fileOut.close();
@@ -104,11 +102,11 @@ public class InformationStorer{
 
     }
 
-    /** Loads the information found in the file into instanceDictionary hashmap **/
+    /** Loads the information found in the file into instanceDictionary HashMap **/
     @SuppressWarnings("unchecked")
-    protected void deserialize(){
+    protected void deserialize() {
 
-        HashMap <String, Object> fileHashMap = new HashMap <String, Object> ();
+        HashMap<String, Object> fileHashMap = new HashMap<String, Object>();
 
         try
         {
@@ -122,11 +120,12 @@ public class InformationStorer{
             for (String key : fileHashMap.keySet())
                 instanceDictionary.put(key, fileHashMap.get(key));
 
-        } catch(IOException i){
+        }
+		catch(IOException i) {
             System.out.println("\nError reading File. Either content was changed or wrong file name. \n");
             i.printStackTrace();
-            
-        } catch(ClassNotFoundException c){
+		}
+		catch(ClassNotFoundException c) {
             System.out.println("\nClass not found\n");
             c.printStackTrace();
         }
@@ -134,22 +133,3 @@ public class InformationStorer{
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
