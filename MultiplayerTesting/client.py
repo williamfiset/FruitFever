094f14afa20f4b124ea107d3b480202eaba1d346
william-fiset <fisetwill@gmail.com>
2014-07-04 00:20:37 -0400

    client & server update

--- MultiplayerTesting/client.py
+++ MultiplayerTesting/client.py
@@ -7,6 +7,7 @@ This script is the client side of the application.
 It is currently functional with the localhost IP but not an external IP
 
 socket DOCS:
+https://docs.python.org/2/howto/sockets.html#socket-howto
 https://docs.python.org/2/library/socket.html
 
 """
@@ -22,8 +23,6 @@ serverIP = raw_input("Enter serverIP Address (or '?' for localhost IP): ").strip
 if serverIP == '?':
 	serverIP = "127.0.0.1"
 
-print 
-
 try:
 
 	client_socket.connect( (serverIP, port) )
@@ -34,3 +33,18 @@ try:
 
 except:
 	print "Could not connect to address: ", serverIP, ":", port, " \n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- MultiplayerTesting/server.py
+++ MultiplayerTesting/server.py
@@ -6,6 +6,7 @@ July, 3, 2014
 This script is the server side of the application.
 
 socket DOCS:
+https://docs.python.org/2/howto/sockets.html#socket-howto
 https://docs.python.org/2/library/socket.html
 
 """
@@ -15,7 +16,9 @@ import socket, sys
 server_socket = socket.socket()
 
  # Perhaps this needs to change if we're connecting to other servers but our own
-server_name = 'localhost'
+
+#           Williams-iMac.local 
+host_name = 'localhost'
 port = 5001  
 
 portAvailable = False
@@ -28,14 +31,17 @@ while not portAvailable:
 		sys.exit(0)
 
 	try:
-		server_socket.bind ( ( server_name, port) ) 
+		server_socket.bind ( ( host_name, port) ) 
 		server_socket.listen(1) # maximum of one connection
 		portAvailable = True
 	except:
 		port += 1
 
-	
+
 print "\nServer is connected to port: ", port, "\n"
+
+# returns 10.255.250.180 as my IP rather than 207.115.103.90
+# print socket.gethostbyname(socket.gethostname()) 
 
 
 while True:
