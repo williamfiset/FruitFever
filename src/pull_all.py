42845ad1e8fb5bd145b2b9856a94eba68135f63a
william-fiset <fisetwill@gmail.com>
2014-08-17 19:31:18 -0400

    Fixed pull_all.py

--- src/pull_all.py
+++ src/pull_all.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3.4
 
 """
 
@@ -22,14 +22,14 @@ if __name__ == '__main__':
 
 	# Read the output into a list
 	branches = output.communicate()[0]
-	branches = branches.split('\n') # not sure if it's \n or \r or even \n\r on windows
+	branches = branches.splitlines() # not sure if it's \n or \r or even \n\r on windows
 
 	pull_branches = []
 
 	# Loop through the branches and find the current branch
 	for branch in branches:
 		if branch is not '':
-
+			branch = branch.decode()
 			# * indicates the current checked-out branch
 			if '*' in branch:
 
@@ -46,14 +46,14 @@ if __name__ == '__main__':
 	git_status_output = subprocess.Popen(['git', 'status'], stdout = subprocess.PIPE).communicate()[0]
 
 	# User did not commit the changes in their current branch, either they stash or commit first
-	if 'nothing to commit, working directory clean' not in git_status_output:
-		print "\nPlease commit or stash your changes before launching this script\n"
+	if b'nothing to commit, working directory clean' not in git_status_output:
+		print ("\nPlease commit or stash your changes before launching this script\n")
 		sys.exit()
 
 	# Loop through the branches and pull em!
 	for branch in pull_branches:
 
-		print
+		print()
 		subprocess.call(['git', 'checkout', branch])
 		subprocess.call(['git', 'pull'])
 	
